3f9ae18b5600041ae76136964e82585fab890cb9
sershishkov <ser.shishkov@gmail.com>
2022-03-29 09:24:56 +0300

    97

--- components/CategoryGridTile.jsx
+++ components/CategoryGridTile.jsx
@@ -1,6 +1,8 @@
 import { View, Pressable, Text, StyleSheet, Platform } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
 
-function CategoryGridTile({ title, color }) {
+function CategoryGridTile({ title, color, onPress }) {
+  // const navigation = useNavigation();
   return (
     <View style={[styles.gridItem]}>
       <Pressable
@@ -9,6 +11,7 @@ function CategoryGridTile({ title, color }) {
           styles.button,
           pressed ? styles.buttonPressed : null,
         ]}
+        onPress={onPress}
       >
         <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -2,13 +2,23 @@ import { FlatList } from 'react-native';
 import CategoryGridTile from '../components/CategoryGridTile';
 import { CATEGORIES } from '../data/dummy-data';
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate('MealsOverView', {
+        categoryId: itemData.item.id,
+      });
+    }
 
-function CategoriesScreen() {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
   return (
     <FlatList
       data={CATEGORIES}
