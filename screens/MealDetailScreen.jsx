7306abfaa2e2f67146cbeb3542e95f06c109ed9a
sershishkov <ser.shishkov@gmail.com>
2022-03-30 10:52:20 +0300

    104

--- screens/MealDetailScreen.jsx
+++ screens/MealDetailScreen.jsx
@@ -1,14 +1,69 @@
 import { useEffect } from 'react';
-
-import { View, Pressable, Text, StyleSheet, Platform } from 'react-native';
+import {
+  View,
+  Pressable,
+  Text,
+  StyleSheet,
+  Platform,
+  Image,
+  ScrollView,
+} from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealDetails from '../components/MealDetails';
+import Subtitle from '../components/mealDetail/Subtitle';
+import List from '../components/mealDetail/List';
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
-    <View>
-      <Text>This is the meal Detail screen ({mealId})</Text>
-    </View>
+    <ScrollView style={styles.rootContainer}>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: 'white',
+  },
+  detailText: {
+    color: 'white',
+  },
+  listOuterContainer: {
+    alignItems: 'center',
+  },
+  listContainer: {
+    width: '80%',
+  },
+});
